049bfe1177548cbdd007afc5d20c5d9a11bcb68c
marcelo-rossi <gesseemail@gmail.com>
2022-01-23 18:06:04 -0300

    Dialogue System
    Desenvolvido o sistema de diálogo

---
+++ Assets/Scripts/Dialogue System/DialogueManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] GameObject _leftDialogueCanvas;
+    [SerializeField] GameObject _RightDialogueCanvas;
+    Dialogue _dialogue;
+    GameObject _endDialogueCanvas;
+    DialogueSide _dialogueSide;
+
+    Sprite _dialoguePortrait;
+    Sprite _dialogueBox;
+    string _dialogueText;
+
+    public void SetDitalogue (Dialogue dialogue)
+    {
+        _dialogue = dialogue;
+    }
+
+    public void StartDialogue()
+    {
+        StartCoroutine(RunDialogue());
+    }
+
+    IEnumerator RunDialogue()
+    {
+        do
+        {
+            if (_dialogue.GetSpeakerSide() == Dialogue.Side.Left)
+            {
+                if (_endDialogueCanvas != null) _endDialogueCanvas.SetActive(false);
+                _endDialogueCanvas = _leftDialogueCanvas;
+            }
+            else
+            {
+                if (_endDialogueCanvas != null) _endDialogueCanvas.SetActive(false);
+                _endDialogueCanvas = _RightDialogueCanvas;
+            }
+            _dialogueSide = _endDialogueCanvas.GetComponent<DialogueSide>();
+            _dialogueSide.SetDialogueBox(_dialogue.GetDialogueBox());
+            _dialogueSide.SetSpeakerPortrait(_dialogue.GetSpeakerPortrait());
+            _dialogueSide.SetSpeakerText(_dialogue.GetSpeakText());
+            if (_dialogue.GetNextDialogue() == null)
+                _dialogueSide.HasMoreDialogue(false);
+            else
+                _dialogueSide.HasMoreDialogue(true);
+            _endDialogueCanvas.SetActive(true);
+
+            
+            yield return new WaitForSeconds(4.0f);
+            if (_dialogue == null)
+            {
+                _endDialogueCanvas.SetActive(false);
+            }
+            else
+            {
+                SetDitalogue(_dialogue.GetNextDialogue());
+            }
+        } while (_dialogue != null);
+    }
+}
